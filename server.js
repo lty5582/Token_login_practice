c9f72c9bb5b10bf0f4fd3ee10c3923a3b1b1f33d
dwqcwda <lty5582@naver.com>
2025-02-03 17:38:44 +0900

    node 4

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:[본인의 포트번호]",
-      "http://localhost:[본인의 포트번호]",
+      "http://127.0.0.1:5501",
+      "http://localhost:5501",
     ],
     methods: ["OPTIONS", "POST", "GET", "DELETE"],
     credentials: true,
@@ -42,8 +42,11 @@ app.post("/", (req, res) => {
   } else {
     // 1. 유저정보가 있는 경우 accessToken을 발급하는 로직을 작성하세요.(sign)
     // 이곳에 코드를 작성하세요.
+    const accessToken = jwt.sign({userId: userInfo.user_id}, secretKey, {expiresIn: 1000 * 60 * 10});
     // 2. 응답으로 accessToken을 클라이언트로 전송하세요. (res.send 사용)
     // 이곳에 코드를 작성하세요.
+    res.cookie('accessToken', accessToken)
+    res.send("로그인 성공");
   }
 });
 
@@ -51,8 +54,16 @@ app.post("/", (req, res) => {
 app.get("/", (req, res) => {
   // 3. req headers에 담겨있는 accessToken을 검증하는 로직을 작성하세요.(verify)
   // 이곳에 코드를 작성하세요.
-  // 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
-  // 이곳에 코드를 작성하세요.
+  const {accessToken} = req.cookies
+  const payload = jwt.verify(accessToken, secretKey);
+  const userInfo = users.find(el => el.user_id === payload.userId);
+  return res.send(userInfo);
 });
+// 4. 검증이 완료되면 유저정보를 클라이언트로 전송하세요.(res.send 사용)
+// 이곳에 코드를 작성하세요.
+app.delete("/", (req, res) => {
+  res.clearCookie('accessToken');
+  res.send("토큰 삭제");
+})
 
 app.listen(3000, () => console.log("서버 실행!"));
